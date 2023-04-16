f68a8629a7a131fc172947035a594e918edd9402
gh_workflow_action <gh_workflow_action@users.noreply.github.com>
2023-04-16 17:57:29 +0000

    update cargo docs

---
+++ docs/implementors/core/panic/unwind_safe/trait.RefUnwindSafe.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"basics":[["impl&lt;'a&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.2/core/panic/unwind_safe/trait.RefUnwindSafe.html\" title=\"trait core::panic::unwind_safe::RefUnwindSafe\">RefUnwindSafe</a> for <a class=\"struct\" href=\"basics/concepts/lifetimes/struct.Cat.html\" title=\"struct basics::concepts::lifetimes::Cat\">Cat</a>&lt;'a&gt;",1,["basics::concepts::lifetimes::Cat"]]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"basics":["",[],["lib.rs"]],\
+"basics":["",[["concepts",[],["lifetimes.rs","mod.rs"]]],["lib.rs"]],\
 "dynamic_programming":["",[["subarrays",[],["max_sum_subarray.rs","mod.rs"]]],["lib.rs"]],\
 "misc":["",[],["lib.rs"]]\
 }');
