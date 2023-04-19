06273de3fe27a9a787efa046aa6b101473079451
gh_workflow_action <gh_workflow_action@users.noreply.github.com>
2023-04-19 17:31:36 +0000

    update cargo docs

--- docs/basics/concepts/sidebar-items.js
+++ docs/basics/concepts/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["lifetimes",""],["static_and_dynamic_dispatch",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["closures",""],["lifetimes",""],["static_and_dynamic_dispatch",""]]};
--- docs/source-files.js
+++ docs/source-files.js
@@ -1,5 +1,5 @@
 var sourcesIndex = JSON.parse('{\
-"basics":["",[["concepts",[],["lifetimes.rs","mod.rs","static_and_dynamic_dispatch.rs"]]],["lib.rs"]],\
+"basics":["",[["concepts",[],["closures.rs","lifetimes.rs","mod.rs","static_and_dynamic_dispatch.rs"]]],["lib.rs"]],\
 "dynamic_programming":["",[["subarrays",[],["max_sum_subarray.rs","mod.rs"]]],["lib.rs"]],\
 "misc":["",[],["lib.rs"]]\
 }');
