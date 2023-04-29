8360077b57e27185ddc1848a652be3b7c2fe813c
suren <suren@git>
2023-04-29 17:52:40 +0100

    rs: closures - fn pointers

--- concepts_rs/src/functional/closures.rs
+++ concepts_rs/src/functional/closures.rs
@@ -6,6 +6,31 @@ fn do_twice(f: fn(i32) -> i32, arg: i32) -> i32 {
     f(arg) + f(arg)
 }
 
+fn closures() {
+    let mut name = String::from("jon");
+
+    let greet = |msg| format!("{} {}", msg, name);
+    println!("{}", greet("hello"));
+
+    // mutable closure that modifies captured variable
+    let mut add_title = |title| {
+        let name_ref = &mut name;
+        // deref to assign new string
+        *name_ref = format!("{} {}", title, name_ref).to_string();
+    };
+    add_title("Mr.");
+    println!("name: {}", &name);
+
+    // exclusive closure. consumes x
+    let mut change_name = move |new_name: &str| {
+        name = new_name.to_string();
+        println!("new name: {}", name);
+    };
+    change_name("Jane");
+    // can't do already moved
+    // println!("new name: {}", name);
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -13,5 +38,6 @@ mod tests {
     fn test() {
         let ans = do_twice(add_one, 5);
         assert_eq!(ans, 12);
+        closures();
     }
 }
