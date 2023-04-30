b17429cb4f5072717adaf2e0552ccd87b5069e7a
suren <suren@git>
2023-04-30 15:08:08 +0100

    rs: strings

--- concepts_rs/src/basics/strings.rs
+++ concepts_rs/src/basics/strings.rs
@@ -1,1 +1,81 @@
+// simple version
+fn is_palindrome(s: &str) -> bool {
+    let s = s.to_string();
+    s == s.chars().rev().collect::<String>()
+}
 
+// better and works with spaces
+fn is_palindrome_v2(s: &str) -> bool {
+    let chars: Vec<char> = s
+        .to_lowercase()
+        .chars()
+        .filter(|x| !x.is_whitespace())
+        .collect();
+    let len = chars.len();
+
+    if len <= 1 {
+        return true;
+    }
+
+    let midway = len / 2;
+    let last_index = len - 1;
+
+    for i in 0..midway {
+        if chars[i] != chars[last_index - i] {
+            return false;
+        }
+    }
+    true
+}
+
+// extra allocation of new string
+fn reverse(s: &mut String) -> &mut String {
+    let reversed: String = s.chars().rev().collect();
+    s.clear();
+    s.push_str(&reversed);
+    s
+}
+
+// using vec
+fn reverse_v2(s: String) -> String {
+    let mut bytes = s.into_bytes();
+    let len = bytes.len();
+    for i in 0..len/2 {
+        bytes.swap(i, len - 1 - i);
+    }
+    let s = String::from_utf8(bytes).unwrap();
+    s
+}
+
+// with references but unsafe
+fn reverse_v3(s: &mut String) -> &mut String {
+    let bytes = unsafe { s.as_bytes_mut() };
+    let len = bytes.len();
+    for i in 0..len/2 {
+        bytes.swap(i, len - 1 - i);
+    }
+    *s = String::from_utf8(bytes.to_vec()).unwrap();
+    s
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test() {
+        let s1 = "racecar";
+        let s2 = "A man a plan a canal Panama";
+        let s3 = "";
+        let s4 = "deed";
+
+        assert_eq!(is_palindrome_v2(s1), true);
+        assert_eq!(is_palindrome_v2(s2), true);
+        assert_eq!(is_palindrome_v2(s3), true);
+        assert_eq!(is_palindrome_v2(s4), true);
+
+        assert_eq!(reverse(&mut String::from("rev")), &mut String::from("ver"));
+        assert_eq!(reverse_v2(String::from("rev")), String::from("ver"));
+        assert_eq!(reverse_v3(&mut String::from("rev")), &mut String::from("ver"));
+    }
+}
--- concepts_rs/test.py
+++
@@ -1,6 +0,0 @@
-def squares(n):
-    for i in range(n):
-        yield i**2 
-
-for x in squares(5):
-    print(x)
