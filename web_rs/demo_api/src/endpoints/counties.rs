e8a2e8b1f1d555b69ca6cd4c412e5d584fe01c24
suren <suren@git>
2023-05-01 06:52:28 +0100

    rs: web - json -remove byte-order-mark. (fixes 'expected value, line:1 column:1' error)

--- web_rs/demo_api/src/endpoints/counties.rs
+++ web_rs/demo_api/src/endpoints/counties.rs
@@ -1,20 +1,43 @@
+use std::{fs, path::PathBuf};
+
 use crate::models::counties::County;
-use axum::{http::StatusCode, Json};
-use std::fs;
-pub async fn all() -> (StatusCode, Json<Vec<County>>) {
-    (StatusCode::OK, Json(load_data().unwrap()))
+use axum::{extract::Path, http::StatusCode, Json};
+use serde::{Deserialize, Serialize};
+use serde_json::Deserializer;
+
+pub async fn all() -> (StatusCode, Json<Vec<Location>>) {
+    (StatusCode::OK, Json(load_json().unwrap()))
 }
 
-fn load_data() -> Result<Vec<County>, serde_json::Error> {
-    let data = fs::read_to_string("./data/UkCounties.json").unwrap();
-    println!("{}", data);
-    let res = serde_json::from_str::<Vec<County>>(&data);
-    if let Err(e) = res {
-        println!("{}", e);
-        Err(e)
-    } else {
-        Ok(res.unwrap())
-    }
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Location {
+    pub name: String,
+    pub abbreviation: String,
+    pub country: String,
+}
+
+fn load_json() -> Result<Vec<Location>, serde_json::Error> {
+    // let data = get_data();
+    // let mut deserializer = Deserializer::from_str(&data);
+    // let counties: Vec<County> = Deserialize::deserialize(&mut deserializer)?;
+    // Ok(counties)
+    let contents = fs::read_to_string("./data/UKCounties.json").expect("file not found");
+
+    let locations: Vec<Location> = serde_json::from_str(&contents).expect("Failed to parse JSON");
+
+    println!("{:#?}", locations);
+    Ok(locations)
+}
+
+fn get_data() -> String {
+    let cwd = std::env::current_dir().unwrap();
+    let filename = PathBuf::from("data/UKCounties.json");
+    let path = cwd.join(filename);
+    println!("{:?}", path);
+
+    let data = std::fs::read_to_string(path).unwrap();
+    println!("retrieved data");
+    data
 }
 
 #[cfg(test)]
@@ -22,7 +45,11 @@ mod tests {
     use super::*;
     #[test]
     fn test() {
-        let d = load_data();
-        //println!("{:?}", d);
+        let d = load_json();
+        if d.is_err() {
+            println!("{}", d.err().unwrap());
+        } else {
+            println!("deserialization successful");
+        }
     }
 }
