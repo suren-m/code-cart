60d6848d98eb77594195535c64ce9c56b8ebfb7f
suren <suren@git>
2023-06-15 20:57:26 +0100

    fizz buzz fuzz and const / enums

--- concepts_go/main.go
+++ concepts_go/main.go
@@ -13,25 +13,4 @@ var days = []string{"sun", "mon", "tue"}
 func main() {
 	fmt.Println(days[Sunday], days[Monday], Tuesday)
 
-	rules := map[int]string{
-		3: "Fizz",
-		5: "Buzz",
-		7: "Fuzz",
-	}
-
-	order := []int{3, 5, 7}
-
-	for i := 1; i <= 100; i++ {
-		output := ""
-		for _, k := range order {
-			if i%k == 0 {
-				output += rules[k]
-			}
-		}
-		if output == "" {
-			fmt.Println(i)
-		} else {
-			fmt.Println(output)
-		}
-	}
 }
